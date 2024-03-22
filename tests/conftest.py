5d1ccebfc562f24269918d27090b88fbf77111fd
Daria Jakuszewicz <d.jakuszewicz@gmail.com>
2024-03-22 15:30:01 +0300

    Добавлен файл conftest для всех тестов

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_management():
+    browser.config.base_url = 'https://demoqa.com'
+    # browser.config.timeout = 40.0
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+
+    yield
+
+    browser.quit()
+
